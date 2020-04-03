53f4a314d03062c2dda40e5399901c93c6a689ef
Nathan Miller <Nathan.J.Miller@nasa.gov>
2020-04-03 14:51:51 -0400

    Removing some old code and fixing test part

--- xspectre/settings/settings.py
+++ xspectre/settings/settings.py
@@ -34,70 +34,8 @@ def load_json(fn):
 
     return data
 
-"""
-class Settings():
-    def __init__(self, fn=None):
-        self.load_default()
-
-        #Currently only reads JSON file
-        if fn is not None:
-            self.load_json(fn)
-
-    def load_default(self):
-        '''Load the default settings. Uses the settings_default.json file in the current
-        directory
-        '''
-
-        self.load_json('settings_default.json')
-           
-        #Just basing this off of what was originally in this code
-        try: 
-            if self.BASE_DIR is None:
-                self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        except:
-            self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    def load_json(self, fn):
-        '''Update the settings based on an input JSON file
-
-        Params
-        ------
-        fn : str
-            filename for the JSON file
-        '''
-
-        with open(fn) as fp:
-            settings = json.load(fp)
-
-        #Settings value will only change from default values
-        #if they exist in the JSON file
-        if 'base_dir' in settings:
-            self.BASE_DIR = settings['base_dir']
-        if 'normalize_flats' in settings:
-            self.NORMALIZE_FLATS = settings['normalize_flats']
-        if 'fits_image_hdu' in settings:
-            self.FITS_IMAGE_HDU = settings['fits_image_hdu']
-        if 'orders' in settings:
-            self.ORDERS = settings['orders']
-        if 'arcs' in settings:
-            self.ARCS = settings['arcs']
-        if 'orders_rimas' in settings:
-            self.ORDERS_RIMAS = settings['orders_rimas']
-
-    def load_xml(self, fn):
-        pass
-
-    def load_txt(self, fn):
-        pass
-
-settings_default = Settings()
-
-"""
-
 settings_default = load_default()
 
 if __name__ == '__main__':
-    #print("settings:", settings.BASE_DIR)
-    #print("arcs:", settings.ARCS[0]['element'])
-    print("settings:", settings['base_dir'])
-    print("arcs:", settings['arcs'][0]['element'])
+    print("settings:", settings_default['base_dir'])
+    print("arcs:", settings_default['arcs'][0]['element'])
